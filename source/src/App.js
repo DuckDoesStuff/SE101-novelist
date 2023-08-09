40c719fb8d88780da6527924bae2d1a27329f182
nhungnguyen1503 <nhung1899@gmail.com>
2023-08-09 19:10:56 +0700

    add styles root

--- source/src/App.js
+++ source/src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
-import './styles/styles.css'
+// import './App.css';
+import 'styles./styles.css'
 
 function App() {
   return (
     <div className="App">
-      <button> </button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

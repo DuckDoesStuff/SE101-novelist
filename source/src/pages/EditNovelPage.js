661deb2adfe496d87ccf77921127e873e8b272bc
DuckDoesStuff <mailtienduc@gmail.com>
2023-08-14 11:32:23 +0700

    Updated edit novel component

--- source/src/pages/EditNovelPage.js
+++ source/src/pages/EditNovelPage.js
@@ -1,11 +1,13 @@
 import React from "react";
-import EditNovel from "../components/EditNovel";
+import EditNovel from "../components/EditNovel/EditNovel";
+import EditChapter from "../components/EditChapter/EditChapter";
 import "../styles/EditNovelPage.css";
 
 const EditNovelPage = () => {
   return (
     <div className="edit-novel-page">
       <EditNovel />
+      <EditChapter />
     </div>
   );
 };

64b0df22a4fdb4fc4bd33d288ff79544ae92e78b
DuckDoesStuff <mailtienduc@gmail.com>
2023-08-16 22:30:27 +0700

    checkout

--- source/src/components/EditChapter/EditChapter.js
+++ source/src/components/EditChapter/EditChapter.js
@@ -1,15 +1,40 @@
 import React from 'react'
 import './EditChapter.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const EditChapter = (props) => {
+  const EditButton = () => {
+    return (
+      <div className='edit-button'>
+        <FontAwesomeIcon icon={faPen} color='white'/>
+      </div>
+    )
+  }
+
+  const DeleteButton = () => {
+    return (
+      <div className='delete-button'>
+        <FontAwesomeIcon icon={faTrashCan} color='white'/>
+      </div>
+    )
+  }
+
 	const ChapterItem = () => {
 		return (
-			<p>Hiiiiii</p>
+			<div className='chapter-item'>     
+        <p style={{marginLeft: '20px'}}>Chapter 1</p>    
+        <div className='buttons'>
+          <EditButton/>
+          <DeleteButton/>
+        </div>
+      </div>
 		)
 	}
 
   return (
-	<div>
+	<div className='edit-chapter'>
+    <h2>Chapters</h2>
 	  <ChapterItem/>
 	</div>
   )

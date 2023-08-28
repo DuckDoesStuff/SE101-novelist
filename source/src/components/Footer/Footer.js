e26119239feeea437ea4105fcabef54f5c8e88bf
nhungnguyen1503 <nhung1899@gmail.com>
2023-08-28 22:52:46 +0700

    Xong footer

--- source/src/components/Footer/Footer.js
+++ source/src/components/Footer/Footer.js
@@ -1,31 +1,69 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return (
-    <div className={styles["footer"]}>
-        <div className={styles["frame"]}>
-            <p className={styles["title"]}> About Us </p>
-            <Link to={'https://github.com/nhungnguyen1503'} className={styles["name"]}>
-            <i class="fa-brands fa-github"></i><p>nhungnguyen1503</p>
-            </Link>
-            <Link to={'https://github.com/FuandB'} className={styles["name"]}>
-            <i class="fa-brands fa-github"></i><p>FuandB</p>
-            </Link>
-            <Link to={'https://github.com/hoangtukbl'} className={styles["name"]}>
-            <i class="fa-brands fa-github"></i><p>hoangtukbl</p>
-            </Link>
-            <Link to={'https://github.com/hnhnguyn'} className={styles["name"]}>
-            <i class="fa-brands fa-github"></i><p>hnhnguyn</p>
-            </Link>
-            <Link to={'https://github.com/DuckDoesStuff'} className={styles["name"]}>
-            <i class="fa-brands fa-github"></i><p>DuckDoesStuff</p>
-            </Link>
+    return (
+        <div className={styles["footer"]}>
+          <p className={styles["title"]}> About Us </p>
+            <div className={styles["frame"]}>
+                <Link
+                    to={"https://github.com/FuandB"}
+                    className={styles["name"]}
+                >
+                    <i class="fa-brands fa-github"></i>
+                    <p>FuandB</p>
+                </Link>
+                <Link
+                    to={"https://github.com/hoangtukbl"}
+                    className={styles["name"]}
+                >
+                    <i class="fa-brands fa-github"></i>
+                    <p>hoangtukbl</p>
+                </Link>
+            </div>
+            <div className={styles["frame"]}>
+                <Link
+                    to={"https://github.com/DuckDoesStuff"}
+                    className={styles["name"]}
+                >
+                    <i class="fa-brands fa-github"></i>
+                    <p>DuckDoesStuff</p>
+                </Link>
+                <Link
+                    to={"https://github.com/nhungnguyen1503"}
+                    className={styles["name"]}
+                >
+                    <i class="fa-brands fa-github"></i>
+                    <p>nhungnguyen1503</p>
+                </Link>
+            </div>
+            <div className={styles["frame"]}>
+                <Link
+                    to={"https://github.com/hnhnguyn"}
+                    className={styles["name"]}
+                >
+                    <i class="fa-brands fa-github"></i>
+                    <p>hnhnguyn</p>
+                </Link>
+            </div>
+            <div className={styles["frame-logo"]}>
+                <p className={styles["HCMUS"]}>
+                    {" "}
+                    From Ho Chi Minh University of Science{" "}
+                </p>
+                <div className={styles["logo-fit"]}>
+                    <div className={styles["logo-hcmus"]}>
+                        <img
+                            src="hcmus-logo-white.png"
+                            className={styles["logo-hcmus"]}
+                        ></img>
+                    </div>
+                    <p className={styles["fit-hcmus"]}> fit@hcmus</p>
+                </div>
+            </div>
         </div>
-        <p className={styles["HCMUS"]}> From Ho Chi Minh University of Science </p>
-    </div>
-  )
-}
+    );
+};
 
 export default Footer;

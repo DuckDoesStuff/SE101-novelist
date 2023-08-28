8f967affe6f902a27e71db0cdaac9e1eed29c07b
nhungnguyen1503 <nhung1899@gmail.com>
2023-08-28 16:12:49 +0700

    checkout

--- source/src/App.js
+++ source/src/App.js
@@ -27,128 +27,128 @@ import Nhun from "./components/Nhun";
 
 
 function App() {
-  // const [isDark, setTheme] = useState(false);
-  // const switchTheme = () => {
-  //   setTheme((prevTheme) => !prevTheme);
-  // };
+  const [isDark, setTheme] = useState(false);
+  const switchTheme = () => {
+    setTheme((prevTheme) => !prevTheme);
+  };
 
-  // const test = (novel) => {
-  //   console.log(novel.id);
-  // };
-  // const testItem = () => {
-  //   console.log("ok");
-  // };
+  const test = (novel) => {
+    console.log(novel.id);
+  };
+  const testItem = () => {
+    console.log("ok");
+  };
 
-  // const author = [
-  //   {
-  //     id: "1",
-  //     username: "Lan Ho Diep 123 hahahahaaha",
-  //     followers: 100,
-  //     novels: 10,
-  //     ava: "ava.jpg",
-  //   },
-  //   {
-  //     id: "2",
-  //     username: "Lan Ho Diep 123",
-  //     followers: 100,
-  //     novels: 10,
-  //     ava: "ava.jpg",
-  //   },
-  //   {
-  //     id: "3",
-  //     username: "Lan Ho Diep 123",
-  //     followers: 100,
-  //     novels: 10,
-  //     ava: "ava.jpg",
-  //   },
-  // ];
+  const author = [
+    {
+      id: "1",
+      username: "Lan Ho Diep 123 hahahahaaha",
+      followers: 100,
+      novels: 10,
+      ava: "ava.jpg",
+    },
+    {
+      id: "2",
+      username: "Lan Ho Diep 123",
+      followers: 100,
+      novels: 10,
+      ava: "ava.jpg",
+    },
+    {
+      id: "3",
+      username: "Lan Ho Diep 123",
+      followers: 100,
+      novels: 10,
+      ava: "ava.jpg",
+    },
+  ];
 
-  // const novel = [
-  //   {
-  //     id: "1",
-  //     title: "Con ga trong lang thang nhieu mau that ngo hahahaha",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Tinh yeu", "Kich tinh"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "2",
-  //     title: "Lan Ho Diep 123",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Tinh yeu"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "3",
-  //     title: "Mot cau chuyen gi do",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Kich tinh"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "4",
-  //     title: "Mot cau chuyen gi do",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Kich tinh"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "5",
-  //     title: "Mot cau chuyen gi do",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Kich tinh"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "6",
-  //     title: "Con ga trong lang thang nhieu mau that ngo",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Dammy"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  //   {
-  //     id: "7",
-  //     title: "Lan Ho Diep 123",
-  //     thumbnail: "image.jpg",
-  //     genre: ["Tinh yeu"],
-  //     status: "",
-  //     content: [""],
-  //     like: 0,
-  //     view: 0,
-  //     comment_section: "",
-  //     author: "haha",
-  //   },
-  // ];
+  const novel = [
+    {
+      id: "1",
+      title: "Con ga trong lang thang nhieu mau that ngo hahahaha",
+      thumbnail: "image.jpg",
+      genre: ["Tinh yeu", "Kich tinh"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "2",
+      title: "Lan Ho Diep 123",
+      thumbnail: "image.jpg",
+      genre: ["Tinh yeu"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "3",
+      title: "Mot cau chuyen gi do",
+      thumbnail: "image.jpg",
+      genre: ["Kich tinh"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "4",
+      title: "Mot cau chuyen gi do",
+      thumbnail: "image.jpg",
+      genre: ["Kich tinh"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "5",
+      title: "Mot cau chuyen gi do",
+      thumbnail: "image.jpg",
+      genre: ["Kich tinh"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "6",
+      title: "Con ga trong lang thang nhieu mau that ngo",
+      thumbnail: "image.jpg",
+      genre: ["Dammy"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+    {
+      id: "7",
+      title: "Lan Ho Diep 123",
+      thumbnail: "image.jpg",
+      genre: ["Tinh yeu"],
+      status: "",
+      content: [""],
+      like: 0,
+      view: 0,
+      comment_section: "",
+      author: "haha",
+    },
+  ];
   // return (
 	// <div>
   //   <Routes>
@@ -173,7 +173,7 @@ function App() {
       <Route path="" element={<HomePage />} />
       <Route path="homepage" element={<HomePage />} />
       <Route path="setting" element={<SettingPage />} />
-
+      <Route path="novel/:id" element={<NovelViewPage />} />
 
     </Routes>
 	</div>
--- source/src/pages/NovelViewPage.js
+++ source/src/pages/NovelViewPage.js
@@ -1,25 +1,25 @@
 import React from "react";
-import Header from "../components/Header/Header";
+import Header from "../components/Header/Header.js";
 import ViewCard from "../components/NovelViewCard/ViewCard";
 
 function NovelViewPage() {
     const container = {
-        padding: '32px 0',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
+        padding: "32px 0",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
     };
 
     return (
         <div>
-            <Header/>
+            <Header />
             <div style={container}>
-                <ViewCard/>
+                <ViewCard />
             </div>
         </div>
     );
-};
+}
 
-export default NovelViewPage
+export default NovelViewPage;

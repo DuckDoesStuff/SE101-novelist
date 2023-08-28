73ca0c3b7ed6775c0cca5ef8167f640de9c73aa5
hnhnguyn <nguynhnghnh1910@gmail.com>
2023-08-28 14:19:31 +0700

    view card update

--- source/src/App.js
+++ source/src/App.js
@@ -18,145 +18,150 @@ import SignInPage from "./pages/SignInPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import HomePage from "./pages/HomePage";
 import SearchPage from "./pages/SearchPage";
+import NovelViewPage from "./pages/NovelViewPage";
 import Header from "./components/Header/Header";
 import SettingPage from "./pages/SettingPage";
 
 
 
 function App() {
-  const [isDark, setTheme] = useState(false);
-  const switchTheme = () => {
-    setTheme((prevTheme) => !prevTheme);
-  };
+  // const [isDark, setTheme] = useState(false);
+  // const switchTheme = () => {
+  //   setTheme((prevTheme) => !prevTheme);
+  // };
 
-  const test = (novel) => {
-    console.log(novel.id);
-  };
-  const testItem = () => {
-    console.log("ok");
-  };
+  // const test = (novel) => {
+  //   console.log(novel.id);
+  // };
+  // const testItem = () => {
+  //   console.log("ok");
+  // };
 
-  const author = [
-    {
-      id: "1",
-      username: "Lan Ho Diep 123 hahahahaaha",
-      followers: 100,
-      novels: 10,
-      ava: "ava.jpg",
-    },
-    {
-      id: "2",
-      username: "Lan Ho Diep 123",
-      followers: 100,
-      novels: 10,
-      ava: "ava.jpg",
-    },
-    {
-      id: "3",
-      username: "Lan Ho Diep 123",
-      followers: 100,
-      novels: 10,
-      ava: "ava.jpg",
-    },
-  ];
+  // const author = [
+  //   {
+  //     id: "1",
+  //     username: "Lan Ho Diep 123 hahahahaaha",
+  //     followers: 100,
+  //     novels: 10,
+  //     ava: "ava.jpg",
+  //   },
+  //   {
+  //     id: "2",
+  //     username: "Lan Ho Diep 123",
+  //     followers: 100,
+  //     novels: 10,
+  //     ava: "ava.jpg",
+  //   },
+  //   {
+  //     id: "3",
+  //     username: "Lan Ho Diep 123",
+  //     followers: 100,
+  //     novels: 10,
+  //     ava: "ava.jpg",
+  //   },
+  // ];
 
-  const novel = [
-    {
-      id: "1",
-      title: "Con ga trong lang thang nhieu mau that ngo hahahaha",
-      thumbnail: "image.jpg",
-      genre: ["Tinh yeu", "Kich tinh"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "2",
-      title: "Lan Ho Diep 123",
-      thumbnail: "image.jpg",
-      genre: ["Tinh yeu"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "3",
-      title: "Mot cau chuyen gi do",
-      thumbnail: "image.jpg",
-      genre: ["Kich tinh"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "4",
-      title: "Mot cau chuyen gi do",
-      thumbnail: "image.jpg",
-      genre: ["Kich tinh"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "5",
-      title: "Mot cau chuyen gi do",
-      thumbnail: "image.jpg",
-      genre: ["Kich tinh"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "6",
-      title: "Con ga trong lang thang nhieu mau that ngo",
-      thumbnail: "image.jpg",
-      genre: ["Dammy"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-    {
-      id: "7",
-      title: "Lan Ho Diep 123",
-      thumbnail: "image.jpg",
-      genre: ["Tinh yeu"],
-      status: "",
-      content: [""],
-      like: 0,
-      view: 0,
-      comment_section: "",
-      author: "haha",
-    },
-  ];
+  // const novel = [
+  //   {
+  //     id: "1",
+  //     title: "Con ga trong lang thang nhieu mau that ngo hahahaha",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Tinh yeu", "Kich tinh"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "2",
+  //     title: "Lan Ho Diep 123",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Tinh yeu"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "3",
+  //     title: "Mot cau chuyen gi do",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Kich tinh"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "4",
+  //     title: "Mot cau chuyen gi do",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Kich tinh"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "5",
+  //     title: "Mot cau chuyen gi do",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Kich tinh"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "6",
+  //     title: "Con ga trong lang thang nhieu mau that ngo",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Dammy"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  //   {
+  //     id: "7",
+  //     title: "Lan Ho Diep 123",
+  //     thumbnail: "image.jpg",
+  //     genre: ["Tinh yeu"],
+  //     status: "",
+  //     content: [""],
+  //     like: 0,
+  //     view: 0,
+  //     comment_section: "",
+  //     author: "haha",
+  //   },
+  // ];
+  // return (
+	// <div>
+  //   <Routes>
+  //     <Route path="search" element={<SearchPage />} />
+  //     <Route path="signup" element={<SignUpPage />} />
+  //     <Route path="signin" element={<SignInPage />} />
+  //     <Route path="forgot" element={<ForgotPasswordPage />} />
+  //     <Route path="" element={<HomePage />} />
+
+  //   </Routes>
+	// </div>
+  // );
+
   return (
-	<div>
-    <Routes>
-      <Route path="search" element={<SearchPage />} />
-      <Route path="signup" element={<SignUpPage />} />
-      <Route path="signin" element={<SignInPage />} />
-      <Route path="forgot" element={<ForgotPasswordPage />} />
-      <Route path="" element={<HomePage />} />
-
-    </Routes>
-	</div>
+    <NovelViewPage/>
   );
 }
 
